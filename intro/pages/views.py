fa17f9610fafc76a2783fa919b3bce93734570cf
Tae-jung <taehee7590@naver.com>
2020-05-12 00:20:28 +0900

    Django Study Day1

--- intro/pages/views.py
+++ intro/pages/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def hello(request):
+    return render(request, 'hello.html')
+
+def lotto(request):
+    pick = random.sample(range(1,46), 6)
+    context = {
+        'pick' : pick
+    }
+    return render(request, 'lotto.html', context)
