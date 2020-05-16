86a84c2532fe96022336601684fafd6f7d73ccad
Tae-jung <taehee7590@naver.com>
2020-05-16 09:12:09 +0900

    Django Study Day3

--- intro/boards/views.py
+++ intro/boards/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'boards/index.html')
+
+def new(request):
+    return render(request, 'boards/new.html')
+
+def complete(request):
+    # request는 요청과 관련된 정보들이 담긴 객체이다.
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    context = {
+        'title' : title,
+        'content' : content,
+    }
+    return render(request, 'boards/complete.html', context)
