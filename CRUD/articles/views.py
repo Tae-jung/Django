8c17f580b6613f8343afe235fd7fa9f70e3b03c7
Tae-jung <taehee7590@naver.com>
2020-06-01 15:51:16 +0900

    CRUD Error

--- CRUD/articles/views.py
+++ CRUD/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
 
 # Create your views here.
@@ -20,26 +20,30 @@ def create(request):
     return redirect('articles:detail', article.pk)
 
 def detail(request, article_pk):
-    article = Article.objects.get(id=article_pk)
+    # article = Article.objects.get(id=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
     context = {
         'article' : article
     }
     return render(request, 'articles/detail.html', context)
 
 def delete(request, article_pk):
-    article = Article.objects.get(id=article_pk)
+    # article = Article.objects.get(id=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
     article.delete()
     return redirect('articles:index')
 
 def edit(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
     context = {
         'article' : article
     }
     return render(request, 'articles/edit.html', context)
 
 def update(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
     article.title = request.POST.get('title')
     article.content = request.POST.get('content')
     article.save()
