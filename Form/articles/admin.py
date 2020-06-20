8b33443c989f7498b2cbd091e01406b035a65df6
Tae-jung <taehee7590@naver.com>
2020-06-20 15:50:45 +0900

    Form/articles Index and admin

--- Form/articles/admin.py
+++ Form/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+# Admin Site를 이쁘게
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at', 'updated_at')
+
+# 어드민 사이트에 등록해줘
+admin.site.register(Article, ArticleAdmin)
--- Form/articles/views.py
+++ Form/articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-pk')  # 가장 최신에 등록한 글을 맨 위로
+    context = {
+        'articles' : articles
+    }
+    return render(request, 'articles/index.html', context)
