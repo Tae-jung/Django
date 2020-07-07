6fbb82adba9bcd6c4cd9732608d36e36c74a4ad6
Tae-jung <taehee7590@naver.com>
2020-07-07 17:50:06 +0900

    instagram Article Model

--- instagram/articles/admin.py
+++ instagram/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display_links = ['title']
+    list_filter = ['created_at']
+
+
+admin.site.register(Article, ArticleAdmin)
